2bd0fd116f9f5e56d908d9748aaaf638ffbd0ea3
gabriel leoni <gabeamaleoni@gmail.com>
2015-09-24 16:54:41 -0700

    Add journal index, new, create, and show controller actions and views

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,10 +1,28 @@
 class JournalsController < ApplicationController
 	def index
+		@journals = Journal.all.order("created_at DESC")
 	end
 
 	def new
+		@journal = Journal.new
+	end
+
+	def create
+		@journal = Journal.new(journal_params)
+		if @journal.save
+			redirect_to @journal
+		else
+			render 'new'
+		end
 	end
 
 	def show
+		@journal = Journal.find(params[:id])
 	end
+
+	private
+
+		def journal_params
+			params.require(:journal).permit(:title, :description)
+		end
 end
