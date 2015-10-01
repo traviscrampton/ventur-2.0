e555663b6b6189090231817b1aa9335561d66fcc
gabriel leoni <gabeamaleoni@gmail.com>
2015-10-01 12:29:46 -0700

    Add links to author profile from index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, password_length: 6..128
-  has_many :journals
+  has_many :journals, dependent: :destroy
 end
