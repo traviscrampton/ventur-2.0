c784d789fe5e51bc21697eafb5d3831da53e2db5
gabriel leoni <gabeamaleoni@gmail.com>
2015-10-06 17:10:11 -0700

    Add storage :s3 to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, password_length: 6..128
   has_many :journals, dependent: :destroy
 
-  has_attached_file :profile, styles: { large: "600x600>", medium: "300x300#", thumb: "150x150#" }, default_url: "profile_img_default.png"
-  validates_attachment_content_type :profile, content_type: /\Aimage\/.*\Z/
+  has_attached_file :profile, 
+  	styles: { 
+  		large: "600x600>", 
+  		medium: "300x300#", 
+  		thumb: "150x150#" }, 
+  		default_url: "profile_img_default.png"
+  		storage: :s3,
+    	s3_credentials: "#{Rails.root}/config/s3.yml"
+  validates_attachment_content_type :profile, 
+  	content_type: /\Aimage\/.*\Z/
 end
