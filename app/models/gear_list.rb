2b957f67e38b76c5415978f79ea225afb74ab868
gabriel leoni <gabeamaleoni@gmail.com>
2015-10-21 14:02:48 -0700

    fixed merge conflicts for gear list

--- app/models/gear_list.rb
+++ app/models/gear_list.rb
@@ -2,10 +2,6 @@ class GearList < ActiveRecord::Base
   belongs_to :journal
   belongs_to :user
   has_many :gear_items
-
-<<<<<<< HEAD
-  validates :title, presence: true, length: { maximum: 30 }
-=======
+  
   validates :title, presence: true, length: { maximum: 30}
->>>>>>> gear-list
 end
