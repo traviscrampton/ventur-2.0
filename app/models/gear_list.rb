a6ebb5ac3794ab3d41b9a39f1982e78848f6d8f4
gabriel leoni <gabeamaleoni@gmail.com>
2015-10-22 12:23:24 -0700

    Added dependent destroy for gear_list gear_items

--- app/models/gear_list.rb
+++ app/models/gear_list.rb
@@ -1,7 +1,7 @@
 class GearList < ActiveRecord::Base
   belongs_to :journal
   belongs_to :user
-  has_many :gear_items
+  has_many :gear_items, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 30}
 end
