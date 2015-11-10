885a755f02fbb2d74ab551502efcb19956201887
gabriel leoni <gabeamaleoni@gmail.com>
2015-11-09 18:34:04 -0800

    added gear list, the cause, and funds raised links to journal#show

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,7 +2,7 @@ class Journal < ActiveRecord::Base
 	belongs_to :user
 	validates :user_id, presence: true
 	validates :title, presence: true, length: { maximum: 50 }
- 	validates :description, presence: true, length: { maximum: 150 }
+ 	validates :description, presence: true, length: { maximum: 200 }
 
 	has_attached_file :journal_image,
 	styles: { 
